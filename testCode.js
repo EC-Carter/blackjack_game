f38c26b7ee723645b0cf960c61dde1af39e396dd
EC Carter <eca21988@gmail.com>
2021-05-05 17:04:37 -0400

    finished game - still working on custom alert

---
+++ testCode.js
@@ -0,0 +1,19 @@
+
+
+//test hands
+
+// let testNoAce =[{rank: 3, suit: "hearts", img: "images/3_of_hearts.png", points: 3},
+// {rank: "king", suit: "clubs", img: "images/king_of_clubs.png", points: 10},
+// {rank: 4, suit: "clubs", img: "images/4_of_clubs.png", points: 4},
+// {rank: 7, suit: "spades", img: "images/7_of_spades.png", points: 7}];
+
+// let testAce =[{rank: "ace", suit: "diamonds", img: "images/ace_of_diamonds.png", points: 1, altPoints: 11},
+// {rank: 3, suit: "clubs", img: "images/3_of_clubs.png", points: 3},
+// {rank: "queen", suit: "diamonds", img: "images/queen_of_diamonds.png", points: 10},
+// {rank: 4, suit: "spades", img: "images/4_of_spades.png", points: 4},]
+
+// let testAce2 =[
+// {rank: 4, suit: "clubs", img: "images/4_of_clubs.png", points: 4},
+// {rank: 7, suit: "spades", img: "images/7_of_spades.png", points: 7},
+// {rank: "ace", suit: "diamonds", img: "images/ace_of_diamonds.png", points: 1, altPoints: 11},
+// ]
